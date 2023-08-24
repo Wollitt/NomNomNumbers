341ad3a0280545d79293a9becbf727ee4bbf1950
AdamPio <adam.piorkowskiyes@gmail.com>
2023-08-24 12:14:21 +0200

    init

---
+++ src/main/java/com/Wollit/NomNomNumbers/NomNomNumbersApplication.java
@@ -0,0 +1,13 @@
+package com.Wollit.NomNomNumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomNomNumbersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomNomNumbersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Wollit/NomNomNumbers/NomNomNumbersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wollit.NomNomNumbers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomNomNumbersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
